76cce4ad332a5857e60b43bee18decc804931f34
Thanakij Pechprasarn <pechpras@gmail.com>
2024-12-19 11:38:20 +0700

    chore: add @stylistic/eslint-plugin and config basic eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint'
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      'indent': ['error', 2],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/max-len': ['error', { 'code': 120, 'tabWidth': 2 }],
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/semi': ['error', 'never'],
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-];
+]
