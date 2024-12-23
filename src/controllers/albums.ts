9320922ee2da01a7a1b008cd3e5bbb461e76f661
Thanakij Pechprasarn <pechpras@gmail.com>
2024-12-23 21:55:17 +0700

    feat: impl albums endpoints using drizzle-orm for DB

--- src/controllers/albums.ts
+++ src/controllers/albums.ts
@@ -1,23 +1,48 @@
 import type { Context } from 'hono'
 
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { env } from 'hono/adapter'
 import { HTTPException } from 'hono/http-exception'
 
 import type { ListAlbumsParam, Id, AlbumIn } from '@/types/albums'
 
+import { DbAlbumRepository } from '@/repositories/db/albums'
 import { MockAlbumRepository } from '@/repositories/mock/albums'
 
 export async function listAlbums(c: Context) {
   // @ts-expect-error not typed well
   const query: ListAlbumsParam = c.req.valid('query')
   console.log(query)
-  const albumRepository = new MockAlbumRepository()
+  const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = env<{
+    DB_HOST: string,
+    DB_USER: string,
+    DB_PASS: string,
+    DB_NAME: string,
+  }>(c)
+  const DATABASE_URL = `postgresql://${DB_USER}:${DB_PASS}@${DB_HOST ?? 'localhost'}:5432/${DB_NAME}`
+  console.log(DATABASE_URL)
+  const db = drizzle({ connection: DATABASE_URL, logger: true })
+  console.log(db)
+  const albumRepository = new DbAlbumRepository(db)
+  //const albumRepository = new MockAlbumRepository()
   return c.json(await albumRepository.list(query), 200)
 }
 
 export async function getAlbum(c: Context) {
   const id = c.req.param('id') as Id
   console.log(id)
-  const albumRepository = new MockAlbumRepository()
+  const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = env<{
+    DB_HOST: string,
+    DB_USER: string,
+    DB_PASS: string,
+    DB_NAME: string,
+  }>(c)
+  const DATABASE_URL = `postgresql://${DB_USER}:${DB_PASS}@${DB_HOST ?? 'localhost'}:5432/${DB_NAME}`
+  console.log(DATABASE_URL)
+  const db = drizzle({ connection: DATABASE_URL, logger: true })
+  console.log(db)
+  const albumRepository = new DbAlbumRepository(db)
+  //const albumRepository = new MockAlbumRepository()
   const album = await albumRepository.get(id)
   if (!album) throw new HTTPException(404, { message: 'Album not found' })
   return c.json(album, 200)
@@ -27,7 +52,18 @@ export async function createAlbum(c: Context) {
   // @ts-expect-error not typed well
   const album: AlbumIn = c.req.valid('json')
   console.log(album)
-  const albumRepository = new MockAlbumRepository()
+  const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = env<{
+    DB_HOST: string,
+    DB_USER: string,
+    DB_PASS: string,
+    DB_NAME: string,
+  }>(c)
+  const DATABASE_URL = `postgresql://${DB_USER}:${DB_PASS}@${DB_HOST ?? 'localhost'}:5432/${DB_NAME}`
+  console.log(DATABASE_URL)
+  const db = drizzle({ connection: DATABASE_URL, logger: true })
+  console.log(db)
+  const albumRepository = new DbAlbumRepository(db)
+  //const albumRepository = new MockAlbumRepository()
   try {
     const id = await albumRepository.create(album)
     return c.json({ id }, 201, { Location: `/albums/${id}` })
---
+++ src/repositories/db/albums.ts
@@ -0,0 +1,45 @@
+import type { NodePgDatabase } from 'drizzle-orm/node-postgres'
+
+import { count, eq, like } from 'drizzle-orm'
+
+import type { AlbumRepository } from '@/repositories/albums'
+import type { ListAlbumsParam, ListAlbums, Id, Album, AlbumIn } from '@/types/albums'
+
+import { albums } from '@/db/schemas'
+
+export class DbAlbumRepository implements AlbumRepository {
+  private db: NodePgDatabase
+
+  public constructor(db: NodePgDatabase) {
+    this.db = db
+  }
+
+  async list(param: ListAlbumsParam): Promise<ListAlbums> {
+    const results = await this.db
+      .select()
+      .from(albums)
+      .where(like(albums.name, `%${param.q}%`))
+      .orderBy(albums.name)
+      .limit(param.limit)
+      .offset(param.offset)
+    console.log(results)
+    const data: Album[] = results.map((each) => ({ id: each.uuid as Id, name: each.name }))
+    const result = await this.db.select({ total: count() }).from(albums).where(like(albums.name, `%${param.q}%`))
+    const total = result[0].total
+    return { meta: { total, count: data.length }, data }
+  }
+
+  async get(id: Id): Promise<Album | null> {
+    const results = await this.db
+      .select()
+      .from(albums)
+      .where(eq(albums.uuid, id))
+      .limit(1)
+    const data: Album[] = results.map((each) => ({ id: each.uuid as Id, name: each.name }))
+    return data.length > 0 ? data[0] : null
+  }
+
+  async create(input: AlbumIn): Promise<Id> {
+    throw new Error('Not implemented')
+  }
+}
