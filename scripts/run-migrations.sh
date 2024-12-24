cd8a64b6eefe9d9154e93f185b1ce576eedc5461
Thanakij Pechprasarn <pechpras@gmail.com>
2024-12-24 21:40:35 +0700

    refactor: drop the need for DB_TYPE

--- scripts/run-migrations.sh
+++ scripts/run-migrations.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-DB_TYPE=${DB_TYPE:-postgresql}
 DB_HOST=${DB_HOST:-localhost}
 DB_USER=${DB_USER}
 DB_PASS=${DB_PASS}
 DB_NAME=${DB_NAME}
 
-export DATABASE_URL=${DB_TYPE}://${DB_USER}:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}
+export DATABASE_URL=postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}
 
 npx drizzle-kit migrate
