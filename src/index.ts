bdffb8472c6e8bc8318de4ddd19e938d8780c46e
Thanakij Pechprasarn <pechpras@gmail.com>
2025-01-13 22:48:03 +0700

    refactor: (1) replace vitest and tsx with bun (2) drop the need for @hono/node-server

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from '@hono/node-server'
-
-import app from '@/app'
-
-serve(app, (info) => {
-  console.log(`Listening on http://localhost:${info.port}`)
-  console.log(`Please go to http://localhost:${info.port}/docs for Interactive API Doc`)
-})
